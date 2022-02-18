343349dc02e3fd62168bd1d279c20b54b03af770
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-02-18 03:56:53 -0300

    Add projectsData file
    It contains an array of categories each with an array of projects.

---
+++ projectsData.js
@@ -0,0 +1,78 @@
+let projectsData = [
+  {
+    "title": "Recent projects",
+    "description": "Projects I've worked on after I joined the university.",
+    "projects": [
+      {
+        "title": "Objetivos (Goals)",
+        "description": "A personal project that helps me write about my goals and set to-dos to make progress in them. It was first written em C and then converted in C++. It was a great way for me to learn those languages. I started it in 2019 and it is still in continuous development.",
+        "button": {
+          "title": "Source code",
+          "link": "https://github.com/gustavoM32/ObjetivosCLI"
+        }
+      },
+      {
+        "title": "InternaSUS",
+        "description": "It is a health dashboard developed by the group InterSCity inside my university. This groups builds technologies for smart cities, using the available data to make the city work more efficiently. I was granted a research scholarship to help the development of the plataform from 2020-2021.",
+        "button": {
+          "title": "Source code",
+          "link": "https://gitlab.com/interscity/health-dashboard/internasus/-/tree/master"
+        }
+      }
+    ]
+  },
+  {
+    "title": "JavaScript projects",
+    "description": "Toy projects I've created when I started to learn how to program in JavaScript with the Processing.js library at Khan Academy in 2017-2018. They are in Portuguese.",
+    "projects": [
+      {
+        "title": "Hit Detection",
+        "description": "I was learning about JavaScript and trying to make a hit detection system, ended up creating this minigame.",
+        "button": {
+          "title": "Play",
+          "link": "projects/hit-detection/0.2.3.html"
+        }
+      },
+      {
+        "title": "Good Morning, Seniors!",
+        "description": "The greatest JavaScript project, it is a dashboard showing the current classes, next tests and birthdays, and the time remaining time for my last year of high school to end. This dashboard was a screensaver for our class projector, when the teachers weren't using it.",
+        "button": {
+          "title": "View",
+          "link": "projects/bom-dia-terceirao/bom-dia.html"
+        }
+      },
+      {
+        "title": "Productivity tool",
+        "description": "A tool to help track time doing tasks weekly.",
+        "button": {
+          "title": "View",
+          "link": "projects/produtividade/Produtividade.html"
+        }
+      },
+      {
+        "title": "Tic-tac-toe",
+        "description": "A simple tic-tac-toe game.",
+        "button": {
+          "title": "Play",
+          "link": "projects/jogo-da-velha/0.3.html"
+        }
+      },
+      {
+        "title": "Sorting visualization",
+        "description": "A visualization for random sort followed by bubble sort.",
+        "button": {
+          "title": "View",
+          "link": "projects/sorts/Sorts.html"
+        }
+      },
+      {
+        "title": "Physics collision tests",
+        "description": "An interactive physics simulation. You can create balls (click and hold to start with momentum) that collide with each other. The collisions are perfectly elastic.",
+        "button": {
+          "title": "View",
+          "link": "projects/colision/Colision.html"
+        }
+      }
+    ]
+  }
+];
